b44009562ef5897e3ec7fea5b52bae7a41eb53e0
Aaron Suarez <aaron.j.suarez1@gmail.com>
2018-04-05 20:44:12 -0500

    Add source_code_hash so that changes to the zip file will be recognized

--- main.tf
+++ main.tf
@@ -58,5 +58,6 @@ resource "aws_lambda_function" "time_and_date_function" {
   function_name   = "timeAndDateFunction"
   role            = "${aws_iam_role.iam_for_lambda.arn}"
   handler         = "exports.timeAndDate"
+  source_code_hash = "${base64sha256(file("timeAndDate.zip"))}"
   runtime         = "nodejs6.10"
 }
